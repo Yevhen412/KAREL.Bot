d7c68c70c16c6422101777ea8c642ce9e1258bbf
Yevhen412 <neogen123456789@gmail.com>
2025-07-12 01:46:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from bybit_websocket import BybitWebSocket
 
 async def main():
     ws = BybitWebSocket()
-    async for msg in ws.listen():
-        print(msg)
+    async for event in ws.listen():
+        print(event)
 
 if __name__ == "__main__":
     asyncio.run(main())
