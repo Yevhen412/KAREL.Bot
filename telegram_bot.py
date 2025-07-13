48b14706ba80c83e1e44e3a200b5e8d21b881392
Yevhen412 <neogen123456789@gmail.com>
2025-07-14 01:43:17 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,14 +9,17 @@ async def notify_telegram(message):
     if not BOT_ENABLE:
         print("🔕 Telegram notifications are disabled.")
         return
+
     if not BOT_TOKEN or not CHAT_ID:
         print("Telegram not configured")
         return
+
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     data = {"chat_id": CHAT_ID, "text": message}
+
     try:
         async with aiohttp.ClientSession() as session:
             async with session.post(url, data=data) as resp:
-                print(f"telegram response: {resp.status} - {await resp.text()}")
+                print(f"Telegram response: {resp.status} - {await resp.text()}")
     except Exception as e:
         print(f"Telegram send error: {e}")
