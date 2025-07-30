bbf25c580c17895ec6954ebd97d5559b775748ef
Yevhen412 <neogen123456789@gmail.com>
2025-07-30 22:05:36 +0200

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,5 +1,5 @@
 import asyncio
-from ATR import fetch_btc_candles, calculate_atr
+from ATR import fetch_btc_candles, calculate_attr
 from Step import analyze_candle
 from Correlation import calculate_correlation
 
@@ -10,18 +10,19 @@ async def main():
     df = await fetch_btc_candles()
 
     # Расчёт ATR
-    atr_value = calculate_atr(df)
+    atr_value = calculate_attr(df)
     print(f"ATR = {atr_value:.2f} USDT")
 
     # Анализ текущей 5-минутной свечи
-    candle_passed_half_atr = await analyze_candle(df, atr_value)
+    candle_passed_half_attr = await analyze_candle(df, atr_value)
 
-    # Если свеча прошла более 50% ATR, запускаем расчёт корреляции
-    if candle_passed_half_atr:
+    # Если свеча прошла более 50% ATR, запускаем корреляцию
+    if candle_passed_half_attr:
         print("Свеча превысила 50% ATR. Запуск расчёта корреляции...")
-        await calculate_correlation()
+        other_assets = ["ETHUSDT", "SOLUSDT", "ADAUSDT", "AVAXUSDT", "XRPUSDT", "PEPEUSDT"]
+        await calculate_correlation(df, other_assets)
     else:
-        print("Свеча не превысила 50% ATR. Завершение.")
+        print("Свеча не превысила 50% ATR. Завершаем анализ.")
 
 # Запуск
 if __name__ == "__main__":
