cfaf733728eeaf5a9ade1c6fa02ffb326c470040
Yevhen412 <neogen123456789@gmail.com>
2025-08-06 10:05:53 +0200

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,6 +1,6 @@
 from screen import TokenMonitor
 
-def handle_new_token(token):
+def fetch_new_token(token):
     print(f"[main.py] Обнаружен мемкоин: {token['name']} | Адрес: {token['address']}")
 
 if __name__ == "__main__":
