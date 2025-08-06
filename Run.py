4d26c68f7fbb3f0f1f293ae63102ff1dacf9766f
Yevhen412 <neogen123456789@gmail.com>
2025-08-06 10:10:37 +0200

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,8 +1,8 @@
 from screen import TokenMonitor
 
-def fetch_new_token(token):
+def handle_new_token(token):
     print(f"[main.py] Обнаружен мемкоин: {token['name']} | Адрес: {token['address']}")
 
 if __name__ == "__main__":
-    monitor = TokenMonitor(callback=fetch_new_token)
+    monitor = TokenMonitor(callback=handle_new_token)
     monitor.run()
