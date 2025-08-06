d5b51ca4e1c189a488ac2924b4a01bb10aeb30d0
Yevhen412 <neogen123456789@gmail.com>
2025-08-06 10:09:00 +0200

    Update Run.py

--- Run.py
+++ Run.py
@@ -4,5 +4,5 @@ def fetch_new_token(token):
     print(f"[main.py] Обнаружен мемкоин: {token['name']} | Адрес: {token['address']}")
 
 if __name__ == "__main__":
-    monitor = TokenMonitor(callback=handle_new_token)
+    monitor = TokenMonitor(callback=fetch_new_token)
     monitor.run()
