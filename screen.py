1081f25dc67334b66777e0e8d7ee9e8fabe5c2a0
Yevhen412 <neogen123456789@gmail.com>
2025-08-06 20:05:41 +0200

    Update screen.py

--- screen.py
+++ screen.py
@@ -10,7 +10,7 @@ class DexScreenerScraper:
 
     def fetch_new_pairs(self):
         url = "https://dexscreener.com/new-pairs"
-    headers = {
+        headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
         }
         try:
