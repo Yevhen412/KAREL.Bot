2e40505293107e9db218e97c6799195d528b5cf3
Yevhen412 <neogen123456789@gmail.com>
2025-08-09 13:40:59 +0200

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,10 +1,5 @@
 import asyncio
-from screen import PumpScreen
-
-async def on_new_token(token):
-    # тут дальше будут фильтры/покупка
-    print(f"🆕 MINT: {token['mint']} | TX: {token['tx']}")
+from screen import listen_pumpfun
 
 if __name__ == "__main__":
-    scr = PumpScreen(callback=on_new_token)
-    asyncio.run(scr.run())
+    asyncio.run(listen_pumpfun())
