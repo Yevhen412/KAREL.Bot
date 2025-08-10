c4fb930d15e9b7c937c6f501fa05c79936b52016
Yevhen412 <neogen123456789@gmail.com>
2025-08-10 19:38:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -25,5 +25,4 @@ TELEGRAM_TOKEN = "xxx:yyyy"       # токен бота
 TELEGRAM_CHAT_ID = "123456789"    # твой chat_id
 TG_RATE_LIMIT_SEC = 1.5           # защита от спама (секунд между сообщениями)
 
-# ===== LOGGING =====
-LOG_LEVEL = "INFO"         # INFO / DEBUG
+
