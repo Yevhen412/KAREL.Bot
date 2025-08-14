4de84824001a4dd5f906bca9808f51cb1e9de76b
Yevhen412 <neogen123456789@gmail.com>
2025-08-14 07:43:16 +0200

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-# utils.py
-from datetime import datetime, timezone
-
-def now_ms() -> int:
-    return int(datetime.now(timezone.utc).timestamp() * 1000)
-
-def round_to_tick(price: float, tick: float) -> float:
-    # округление вниз к ближайшему тик-ШАГУ
-    return round((price // tick) * tick, 10)
